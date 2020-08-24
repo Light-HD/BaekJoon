eccbf71f5620e392455b1b7081571956a833e529
gusdyd1432 <68851230+gusdyd1432@users.noreply.github.com>
2020-08-24 22:50:12 +0900

    TSP-DP+비트마스킹

--- TSP-(DP+BITS).cpp
+++ TSP-(DP+BITS).cpp
@@ -9,15 +9,19 @@ int min(int a, int b) { return a < b ? a : b; }
 int tsp(int x, int visit)
 {
 	if (dp[x][visit] > -1) return dp[x][visit];		//dp에 저장된 값이 있으면 그 값을 바로 반환
-	
+	if (visit == (1 << (N - 1)) - 1)			//모든 노드를 다 방문했을 경우
+	{
+		if (W[x][N - 1] != 0) return W[x][N - 1];		//x에서 END 지점까지 길이 있으면 그 길로 가고
+		return INF;								//길이 없으면 무한 반환
+	}
 	int res = INF;					//res를 무한으로 지정
 	for (int i = 0; i < N; i++)
 	{
-		if (visit & (1 << i) || W[x][i] == 0) continue;		//visit(이진수)와(1<<i) 둘다 1이거나 x에서 i까지 길이 없다면 continue
-		res = min(res, tsp(i, visit | (1 << i)) + W[x][i]);		//아니면 res 값을 현재 res 값과 x에서i까지의 경로 값 + tsp 다음꺼랑 비교해서 작은걸 선택
+		if (visit & (1 << i) || W[x][i] == 0) continue;	//방문한곳이거나 길이 없는 경우면 패스
+		res = min(res, tsp(i, visit | (1 << i)) + W[x][i]);	//방문안한곳이고 길이 있으면 거기까지의 거리 + tsp()값중 작은거를 선택
 	}
-	dp[x][visit] = res;			//res 값을 dp에 저장
-	return res;					//res 값을 반환
+	dp[x][visit] = res;
+	return res;
 }
 int main()
 {
