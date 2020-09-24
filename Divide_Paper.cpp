74829250a91d89e986e6e5395a81332d070533cc
gusdyd1432 <68851230+gusdyd1432@users.noreply.github.com>
2020-09-24 22:28:50 +0900

    색종이 만들기
    BaekJoon-2630

--- Divide_Paper.cpp
+++ Divide_Paper.cpp
@@ -8,13 +8,13 @@ void divide(int x, int y, int length) {				//나누어서 정사각형을 판단
 	int wc = 0, bc = 0;
 	for (int i = x; i < x+length; i++) {			//x시작점에서 길이만큼
 		for (int j = y; j < y+length; j++) {		//y시작점에서 길이만큼
-			if (paper[i][j] == 0) wc++;				//0이면 wc ++
-			else bc++;						//1이면 bc ++
+			if (paper[i][j] == 0) wc++;		//0이면 wc ++
+			else bc++;				//1이면 bc ++
 		}
 	}
 	if (wc == length * length) white++;		//wc카운트 했던게 넓이만큼 있으면 0으로 꽉찬것이므로 white 갯수 증가
 	else if (bc == length * length) blue++;		//bc카운트 했던게 넓이만큼 있으면 1로 꽉찬것이므로 blue 갯수 증가
-	else {											//둘다 아니라면 4등분으로 나눔	
+	else {						//둘다 아니라면 4등분으로 나눔	
 		divide(x, y, length / 2);
 		divide(x + (length / 2),y, length / 2);
 		divide(x, y + (length / 2), length / 2);
