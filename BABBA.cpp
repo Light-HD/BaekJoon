6aba10a09505bcf513a15fdb14aec980efe40472
gusdyd1432 <68851230+gusdyd1432@users.noreply.github.com>
2020-10-08 04:43:16 +0900

    BABBA
    BaekJoon-9625

---
+++ BABBA.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int k,temp,A=1,B=0;
+	scanf("%d", &k);
+	for (int i = 0; i < k; i++) {
+		temp = A;	//이전의 A값을 temp에 임시저장
+		A = B;		//A값 업데이트(이전의 B)
+		B += temp;	//B값 업데이트(이전의 A+B)
+	}
+	printf("%d %d", A, B);
+}
