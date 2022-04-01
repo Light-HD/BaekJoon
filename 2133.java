c26c2d3ce70424048278d5dc0ca1f980c2bc8768
Light-HD <68851230+Light-HD@users.noreply.github.com>
2022-04-01 11:06:12 +0900

    타일 채우기
    https://www.acmicpc.net/problem/2133

---
+++ 2133.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = N < 5 ? new int[5] : new int[N+1];
+		dp[2] = 3;
+		dp[4] = 11;
+		if(N >= 6 && N % 2 == 0) {
+			// f(n) = f(n-2) * f(2) + (f(n-4) * 2 + f(n-6) * 2) + 2
+			for (int i = 6; i < dp.length; i+=2) {
+				dp[i] = dp[i-2] * dp[2];
+				for (int j = i-4; j >= 0; j-=2) {
+					dp[i] += dp[j] * 2;
+				}
+				dp[i] += 2;
+			}
+		}
+		System.out.println(dp[N]);
+	}
+}
